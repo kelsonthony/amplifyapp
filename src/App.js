46a8a0ba996996ecc4eb584ea0bf4763df17c1b0
kelsonthony <k.thony@gmail.com>
2022-01-10 15:20:17 -0300

    added auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Kelson and Hello World.</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <h1>WE now have auth</h1>
             </header>
         </div>
     );
 }
 
-export default App;
+export default withAuthenticator(App);
